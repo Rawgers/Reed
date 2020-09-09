7c0021a0d12adced493211e7de6687665f13aea9
Roger Luo <rawger@berkeley.edu>
2020-09-09 02:02:29 -0700

    Initial Commit

---
+++ Reed/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Reed
+//
+//  Created by Roger Luo on 9/9/20.
+//  Copyright © 2020 Roger Luo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
