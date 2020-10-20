a0664bc96b48822754921d02d0ce0a626205ea7c
zhanhugo <42554019+zhanhugo@users.noreply.github.com>
2020-10-20 02:50:25 -0700

    Reader: Implement definition modal. (#9)
    Made a draggable view that uses environmental objects to pass the selected string from the TextView to the DefinitionModal.

--- Reed/Reader/views/DefinitionModal.swift
+++ Reed/Reader/views/DefinitionModal.swift
@@ -10,13 +10,23 @@ import SwiftUI
 
 
 struct DefinitionModal: View {
+    @EnvironmentObject var wordSelection: Word
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.gray
+            Text(self.wordSelection.word)
+        }
+        .edgesIgnoringSafeArea(.vertical)
     }
 }
 
 struct DefinitionModal_Previews: PreviewProvider {
     static var previews: some View {
-        DefinitionModal()
+        DefinitionModal().environmentObject(Word())
     }
+}
+
+class Word: ObservableObject {
+    @Published var word: String = ""
 }
--- Reed/Reader/views/ReaderView.swift
+++ Reed/Reader/views/ReaderView.swift
@@ -13,24 +13,51 @@ import SwiftUIPager
 struct ReaderView: View {
     @ObservedObject var viewModel: ReaderViewModel
     @State var page: Int = 0
+    @State private var currentPosition: CGSize = .zero
+    @State private var newPosition: CGSize = .zero
+
     var body: some View {
-        Pager(page: $page,
-              data: viewModel.items,
-              id: \.self,
-              content: { text in
-                VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+        VStack(alignment: .center, content: {
+            Pager(page: $page,
+                  data: viewModel.items,
+                  id: \.self,
+                  content: { text in
+                    VStack(alignment: .center, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
                         TextView(text: text)
-                        Text("\(page + 1)")
+                    })
+            }).alignment(.start).preferredItemSize(CGSize(
+                width: UIScreen.main.bounds.width,
+                height: UIScreen.main.bounds.height * 0.65
+            ))
+            DefinitionModal()
+                .frame(
+                    width: UIScreen.main.bounds.width * 0.8,
+                    height: UIScreen.main.bounds.height * 0.2
+                ).cornerRadius(20)
+                .offset(
+                    x: self.currentPosition.width,
+                    y: self.currentPosition.height
+                ).gesture(DragGesture().onChanged { value in
+                    self.currentPosition = CGSize(
+                        width: value.translation.width + self.newPosition.width,
+                        height: value.translation.height + self.newPosition.height
+                    )
+                }.onEnded { value in
+                    self.currentPosition = CGSize(
+                        width: value.translation.width + self.newPosition.width,
+                        height: value.translation.height + self.newPosition.height
+                    )
+                    self.newPosition = self.currentPosition
                 })
+            Text("\(page + 1)")
         })
-            .alignment(.start)
-            .preferredItemSize(CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
      }
 }
 
 struct TextView: UIViewRepresentable {
     var text: String!
-    
+    @EnvironmentObject var wordSelection: Word
+
     func makeUIView(context: UIViewRepresentableContext<TextView>) -> UIView {
         let textView = UITextView()
         textView.text = text
@@ -40,8 +67,13 @@ struct TextView: UIViewRepresentable {
     }
 
     class Coordinator: NSObject {
+        var wordSelection: Word
         var tappedRange: UITextRange!
         var selectedRange: NSRange!
+        
+        init(wordSelection: Word) {
+            self.wordSelection = wordSelection
+        }
         
         @objc func wordTapped(gesture: UITapGestureRecognizer) {
             let textView = gesture.view as! UITextView
@@ -50,7 +82,8 @@ struct TextView: UIViewRepresentable {
             let tapPosition = textView.closestPosition(to: position)
             tappedRange = textView.tokenizer.rangeEnclosingPosition(tapPosition!, with: UITextGranularity.word, inDirection: UITextDirection(rawValue: 1))
             if let tappedRange = tappedRange {
-                if let _ = textView.text(in: tappedRange) {
+                if let tappedWord = textView.text(in: tappedRange) {
+                    self.wordSelection.word = tappedWord
                     let locInt = textView.offset(from: textView.beginningOfDocument, to: tappedRange.start)
                     let length = textView.offset(from: tappedRange.start, to: tappedRange.end)
                     if selectedRange != nil {
@@ -64,7 +97,7 @@ struct TextView: UIViewRepresentable {
     }
 
     func makeCoordinator() -> TextView.Coordinator {
-        return Coordinator()
+        return Coordinator(wordSelection: wordSelection)
     }
 
     func updateUIView(_ uiView: UIView,
@@ -74,6 +107,6 @@ struct TextView: UIViewRepresentable {
 
 struct ReaderView_Previews: PreviewProvider {
     static var previews: some View {
-        ReaderView(viewModel: ReaderViewModel())
+        ReaderView(viewModel: ReaderViewModel()).environmentObject(Word())
     }
 }
