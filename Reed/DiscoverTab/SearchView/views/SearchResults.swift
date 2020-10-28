a1703f5bac1b7ebfc0c5b0077c802b14a3dc9804
Roger Luo <rawger@berkeley.edu>
2020-10-28 16:05:01 -0700

    DiscoverTab/SearchView: Implement loading seach result data, view popping.
    This commit implements the functionality for a SearchResults view to load Narou data using the query term. We also change the NavigationView's back button to show "Search" instead of "Discover" because the SearchResults view pops back to the SearchHistory view.

--- Reed/DiscoverTab/SearchView/views/SearchResults.swift
+++ Reed/DiscoverTab/SearchView/views/SearchResults.swift
@@ -7,15 +7,41 @@
 //
 
 import SwiftUI
+import struct SwiftyNarou.NarouRequest
+import struct SwiftyNarou.NarouResponseFormat
 
 struct SearchResults: View {
+    @ObservedObject var viewModel: DiscoverSearchResultsViewModel
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    var btnBack : some View { Button(action: {
+        self.presentationMode.wrappedValue.dismiss()
+        }) {
+            HStack {
+                Image(systemName: "chevron.backward")
+                    .imageScale(.large)
+                Text("Search")
+                    .font(.callout)
+                    .fontWeight(.regular)
+            }
+        }
+    }
+    
     var body: some View {
-        Text("Results!")
+        List {
+            ForEach(viewModel.searchResults, id: \.self) {
+                DiscoverListItem(viewModel: $0)
+            }
+        }
+        .navigationBarTitle(viewModel.keyword, displayMode: .inline)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: btnBack)
     }
 }
 
 struct SearchResults_Previews: PreviewProvider {
     static var previews: some View {
-        SearchResults()
+        SearchResults(viewModel: DiscoverSearchResultsViewModel(keyword: "無職転生"))
     }
 }
+
---
+++ Reed/Extensions/UINavigationController+UIGestureRecognizerDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  UINavigationController+UIGestureRecognizerDelegate.swift
+//  Reed
+//
+//  Created by Roger Luo on 10/28/20.
+//  Copyright © 2020 Roger Luo. All rights reserved.
+//
+
+import SwiftUI
+
+/// Preserves NavigationView left swipe when hidden.
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
