ea330105a73d31d4bc4cfdedc71863deeddad56a
Roger Luo <rawger@berkeley.edu>
2020-11-19 02:49:31 -0800

    Shiori/Tokenizer: Tokenizer Tests
    Adds integration tests for the tokenizer.

---
+++ ReedTests/Tokenizer/TokenizerTests.swift
@@ -0,0 +1,44 @@
+//
+//  TokenizerTests.swift
+//  ReedTests
+//
+//  Created by Shiori Yamashita on 2020/11/16.
+//  Copyright © 2020 Roger Luo. All rights reserved.
+//
+
+import XCTest
+@testable import Reed
+
+class TokenizerTests: XCTestCase {
+    var tokenizer: Tokenizer!
+
+    override func setUp() {
+        super.setUp()
+        tokenizer = Tokenizer()
+    }
+
+    func testTokenize() {
+        let tokens = tokenizer.tokenize("予算ゼロ！？　錬金術協会で基礎研究をしていましたが、退職して学生時代の友人(女騎士)の辺境開発を手伝うことにしました。")
+        printTokens(tokens: tokens)
+    }
+    
+    func printTokens(tokens: [Token]) {
+        for token in tokens {
+            print("=============================")
+            print(token.surface)
+            print("range: " + String(token.range.0) + "-" + String(token.range.1))
+            print("------- mecabWordNodes ------")
+            dump(token.mecabWordNodes)
+            print("----- deinflectionResult ----")
+            dump(token.deinflectionResult)
+            print("--- dictionaryDefinitions ---")
+            for definition in token.dictionaryDefinitions {
+                print(definition.partsOfSpeech.joined(separator: ", "))
+                print(definition.glosses.joined(separator: "; "))
+            }
+            print("--------- furiganas ---------")
+            dump(token.furiganas)
+        }
+    }
+
+}
