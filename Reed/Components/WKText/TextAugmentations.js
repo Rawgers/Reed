557a32bf9dd6f9f25a0ce1129b8fbbd12e2c2594
Roger Luo <rawger@berkeley.edu>
2021-08-12 23:17:12 -0700

    rawger/WKText: Implement word selection.
    Implemented functionality to display definition details in Definer upon tapping word. Also implements functionality to highlight selection, but is buggy.

--- Reed/Components/WKText/TextAugmentations.js
+++ Reed/Components/WKText/TextAugmentations.js
@@ -1,5 +1,5 @@
-// const html = document.documentElement.innerHTML;
-// window.webkit.messageHandlers.handleTapWord.postMessage({ 'everything': html });
+//const html = document.documentElement.innerHTML;
+//window.webkit.messageHandlers.handleTapWord.postMessage({ 'everything': html });
 
 // Add CSS to ReaderView
 const style = document.createElement('style');
@@ -7,26 +7,36 @@ style.textContent = `
     body {
         font-size: 36px; 
     }
-    rt { 
+    rt {
         font-size: 18px;
+    }
+    .highlighted {
+        background-color: #FFFF00;
     }
 `;
 document.head.appendChild(style);
 
-const onTapWord = word => {
+let lastSelectedToken;
+const onTapWord = token => {
     return () => {
         if (window.webkit 
             && window.webkit.messageHandlers 
             && window.webkit.messageHandlers.handleTapWord)
         {
+            if (lastSelectedToken) {
+                lastSelectedToken.classList.remove('highlighted');
+            }
+            
+            const word = token.getAttribute('data-surface');
             window.webkit.messageHandlers.handleTapWord.postMessage({ word });
+            token.classList.add('highlighted');
+            lastSelected = token;
         }
     };
 };
 
 const tokens = document.getElementsByTagName('span');
 for (token of tokens) {
-    const word = token.getAttribute('data-surface');
-    const handleTap = onTapWord(word);
+    const handleTap = onTapWord(token);
     token.onclick = handleTap;
 }
