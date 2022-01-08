8217e10e9af2a4ff80527e80f4265c4fd4d379b0
Hugo Zhan <hugozhan0802@gmail.com>
2022-01-08 00:04:03 -0800

    hugo/WKText: Brought back section navigation
    Added gesture recognizer to webView to toggle navigation menu on and off, disabled user selection and zoom, disable highlighting words via double clicking.

--- Reed/Components/WKText/AddClickHandlers.js
+++ Reed/Components/WKText/AddClickHandlers.js
@@ -4,6 +4,10 @@
 // Add CSS to ReaderView
 const style = document.createElement('style');
 style.textContent = `
+    * {
+        -webkit-touch-callout: none;
+        -webkit-user-select: none
+    }
     body {
         font-size: 36px;
     }
@@ -19,21 +23,32 @@ style.textContent = `
 `;
 document.head.appendChild(style);
 
+// disable zoom
+const meta = document.createElement('meta');
+meta.name = 'viewport';
+meta.content = 'width=device-width, initial-scale=0.5, maximum-scale=0.5, user-scalable=no';
+const head = document.getElementsByTagName('head')[0];
+head.appendChild(meta);
+
 let lastSelectedToken;
+let timer;
 const onTapWord = token => {
     return () => {
-        if (window.webkit 
-            && window.webkit.messageHandlers 
-            && window.webkit.messageHandlers.handleTapWord)
-        {
-            if (lastSelectedToken) {
-                lastSelectedToken.classList.remove('highlighted');
+        if (timer) clearTimeout(timer);
+        timer = setTimeout(() => {
+            if (window.webkit
+                && window.webkit.messageHandlers
+                && window.webkit.messageHandlers.handleTapWord)
+            {
+                if (lastSelectedToken) {
+                    lastSelectedToken.classList.remove('highlighted');
+                }
+                const word = token.getAttribute('data-deinflection');
+                window.webkit.messageHandlers.handleTapWord.postMessage({ word });
+                token.classList.add('highlighted');
+                lastSelectedToken = token;
             }
-            const word = token.getAttribute('data-deinflection');
-            window.webkit.messageHandlers.handleTapWord.postMessage({ word });
-            token.classList.add('highlighted');
-            lastSelectedToken = token;
-        }
+        }, 250);
     };
 };
 
@@ -41,4 +56,7 @@ const tokens = document.getElementsByTagName('span');
 for (token of tokens) {
     const handleTap = onTapWord(token);
     token.onclick = handleTap;
+    token.ondblclick = function() {
+        clearTimeout(timer);
+    }
 }
